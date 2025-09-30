4c267d025139e80d8bd79c6a6364593eb684b5ad
jinnnn-dev <janeplacido09@gmail.com>
2025-09-30 11:53:55 +0800

    feat(integration): finalize SmartFarm demo in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,23 @@
+import 'farm_controller.dart';
+import 'water_pump.dart';
+import 'chicken_coop.dart';
+import 'tractor.dart';
 
 void main() {
-  print("Main demo will go here once parts are merged.");
+  var pump = WaterPump(label: 'EastPump', pressure: 55);
+  var coop = ChickenCoop(label: 'SouthCoop', chickens: 35);
+  var tractor = Tractor(label: 'RedTractor');
+
+  var farm = FarmController(
+    id: 'AgriHub-01',
+    pump: pump,
+    coop: coop,
+    tractor: tractor,
+  );
+
+  print('--- Demo: morningRoutine ---');
+  farm.morningRoutine();
+
+  print('--- Demo: nightRoutine ---');
+  farm.nightRoutine();
 }
