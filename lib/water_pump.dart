ac8ca259ef94f1438fa6554186739b3532aeee71
sofiecarel <sofiearahan@gmail.com>
2025-09-30 11:30:53 +0800

    Update water_pump.dart

--- lib/water_pump.dart
+++ lib/water_pump.dart
@@ -1,1 +1,14 @@
+class WaterPump {
+  String label;
+  int pressure; // PSI
 
+  WaterPump({this.label = 'IrrigationPump', this.pressure = 40});
+
+  void startPump() {
+    print('[$label] startPump -> pressure $pressure PSI');
+  }
+
+  void stopPump() {
+    print('[$label] stopPump');
+  }
+}
